a3825d6643b17a63964edc6155f26490ea8f8aa6
tommyvercetti <Andrian@Andrians-Mac-mini.local>
2016-01-21 14:27:55 +0200

    lesson 10

---
+++ ToDo List/MainTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainTableViewController.h
+//  ToDo List
+//
+//  Created by Andrian on 1/21/16.
+//  Copyright © 2016 Andrian Kryk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTableViewController : UITableViewController
+
+@end
